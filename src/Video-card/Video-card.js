102786703d585de95aa8433d86797880add0b2c8
Nilesh <Nilesh.Kolhe92@gmail.com>
2021-09-17 14:34:25 +0530

    Updated Read Me

--- src/Video-card/Video-card.js
+++ src/Video-card/Video-card.js
@@ -5,13 +5,7 @@ import { pipSharedService } from '../Shared-Service/Shared-service';
 class Videocard extends React.Component {
 
     PlayPIPVideo() {
-        console.log('Play PIP Video')
         pipSharedService.setData(true, this.props.video);
-    }
-
-    handleClick() {
-        console.log('Handle Click')
-        this.setState({active: true});
     }
 
     render() {
@@ -19,15 +13,12 @@ class Videocard extends React.Component {
         return (
             <div className="video-card" onClick={this.PlayPIPVideo.bind(this)}>
                 <div className="thumbnail">
-                {/* <div class="thumbnail"> */}
-                    {/* <a href={`https://www.youtube.com/watch?v=${this.props.video.videoId}`}> */}
                         <img 
                             height={this.props.video.thumbnail.height + 10}
                             width={this.props.video.thumbnail.width}
                             src={this.props.video.thumbnail.url}
                             alt=""
                         />
-                    {/* </a> */}
                 </div>
                 <div className="video-info">
                     <div className="heading"> {this.props.video.title} </div>
